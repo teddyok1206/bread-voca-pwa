5381b2c505284f363468435ea0e44349cdbecc4b
taeuk <teddyok1206@snu.ac.kr>
2026-03-25 04:02:59 +0900

    Deploy Bread VOCA PWA

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "voca-shell-v2";
+const BASE_PATH = self.location.pathname.replace(/[^/]+$/, "");
+const INDEX_URL = `${BASE_PATH}index.html`;
+const SHELL_URLS = [BASE_PATH, INDEX_URL, `${BASE_PATH}manifest.webmanifest`, `${BASE_PATH}icon.svg`];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_URLS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match(INDEX_URL))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const fetchPromise = fetch(event.request)
+        .then((response) => {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || fetchPromise;
+    })
+  );
+});
